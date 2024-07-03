092c0434645d765ea77c9742ddd0554b4ff7fab9
Reiyue <emma900518@gmail.com>
2024-07-03 20:10:53 +0800

    2-3-1
    +換彈夾、文字顯示

--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Shooting : MonoBehaviour
@@ -9,30 +10,87 @@ public class Shooting : MonoBehaviour
     public Transform attackPoint;
 
     [Header("Bullet")]
-    public float pushForce = 50.0f;
     public GameObject bullet;
 
+    [Header("Gun")]
+    public int magazineSize;   
+    public int bulletsLeft;    
+    public float reloadTime;    
+
+    bool reloading;             
+
+    [Header("UI")]
+    public TextMeshProUGUI ammunitionDisplay;
+    public TextMeshProUGUI reloadingDisplay;
+
+    private void Start()
+    {
+        bulletsLeft = magazineSize;        
+        reloadingDisplay.enabled = false;  
+
+        ShowAmmoDisplay();               
+    }
 
     private void Update()
     {
+        MyInput();
+    }
+    private void MyInput()
+    {
         if (Input.GetMouseButtonDown(0) == true)
         {
-            Ray ray = PlayerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)); 
-            RaycastHit hit;  
-            Vector3 targetPoint;  
-
-  
-            if (Physics.Raycast(ray, out hit) == true)
-                targetPoint = hit.point;       
-            else
-                targetPoint = ray.GetPoint(75);  
-
-            Debug.DrawRay(ray.origin, targetPoint - ray.origin, Color.red, 10);
-            Vector3 directionWithoutSpread = targetPoint - attackPoint.position; 
-            GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity); 
-            currentBullet.transform.forward = directionWithoutSpread.normalized; 
-
-            currentBullet.GetComponent<Rigidbody>().AddForce(directionWithoutSpread.normalized * pushForce, ForceMode.Impulse);
+            if (bulletsLeft > 0 && !reloading)
+            {
+                Shoot();
+            }
         }
+
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading)
+            Reload();
+    }
+    private void Shoot()
+    {
+        Ray ray = PlayerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));  
+        RaycastHit hit; 
+        Vector3 targetPoint;  
+
+        if (Physics.Raycast(ray, out hit) == true)
+            targetPoint = hit.point;         
+        else
+            targetPoint = ray.GetPoint(75);  
+
+        Debug.DrawRay(ray.origin, targetPoint - ray.origin, Color.red, 10); 
+
+        Vector3 shootingDirection = targetPoint - attackPoint.position; 
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity); 
+        currentBullet.transform.forward = shootingDirection.normalized; 
+
+        currentBullet.GetComponent<Rigidbody>().AddForce(currentBullet.transform.forward * 20, ForceMode.Impulse);
+
+        bulletsLeft--; 
+
+        ShowAmmoDisplay();                 
+    }
+
+    private void Reload()
+    {
+        reloading = true;                   
+        reloadingDisplay.enabled = true;    
+        Invoke("ReloadFinished", reloadTime);  
+    }
+
+
+    private void ReloadFinished()
+    {
+        bulletsLeft = magazineSize;            
+        reloading = false;                    
+        reloadingDisplay.enabled = false;     
+        ShowAmmoDisplay();
+    }
+
+    private void ShowAmmoDisplay()
+    {
+        if (ammunitionDisplay != null)
+            ammunitionDisplay.SetText($"Ammo {bulletsLeft} / {magazineSize}");
     }
 }
