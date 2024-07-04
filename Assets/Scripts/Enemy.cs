6a5ca1f527d3b507754da0a7162e2d87487711a7
Reiyue <emma900518@gmail.com>
2024-07-04 12:42:49 +0800

    2-6-1
    加入血條跟血條控制 那個提示沒了不知道是不是刪掉敵人的剛體就好了

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
@@ -9,13 +10,19 @@ public class Enemy : MonoBehaviour
     public string targetName = "Player";                    
     public float minimunTraceDistance = 5.0f;
 
-    NavMeshAgent navMeshAgent;
-    GameObject targetObject = null;                                                        
+    [Header("Hp")]
+    public float maxLife = 10.0f;              
+    public Image lifeBarImage;               
+    float lifeAmount;                         
 
+    NavMeshAgent navMeshAgent;
+    GameObject targetObject = null;
+    
     void Start()
     {
         targetObject = GameObject.FindGameObjectWithTag(targetName); 
         navMeshAgent = GetComponent<NavMeshAgent>();
+        lifeAmount = maxLife;
     }
 
     void Update()
@@ -27,10 +34,32 @@ public class Enemy : MonoBehaviour
             navMeshAgent.enabled = true;
         else
             navMeshAgent.enabled = false;
+
+        faceTarget(); 
+
+        if (lifeAmount <= 0.0f)
+            Destroy(gameObject);
     }
 
     void FixedUpdate()
     {
-       navMeshAgent.SetDestination(targetObject.transform.position);
+        if (navMeshAgent.enabled)
+            navMeshAgent.SetDestination(targetObject.transform.position);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Bullet")
+        {
+            lifeAmount -= 1.0f;
+            lifeBarImage.fillAmount = lifeAmount / maxLife;
+        }
+    }
+
+    void faceTarget()
+    {
+        Vector3 targetDir = targetObject.transform.position - transform.position;                               // 計算敵人與角色之間的向量
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, 0.1f * Time.deltaTime, 0.0F);      // 依照敵人Z軸向量與兩者間向量，可以計算出需要旋轉的角度
+        transform.rotation = Quaternion.LookRotation(newDir);                                                   // 進行旋轉
     }
 }
